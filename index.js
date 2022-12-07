6fd9b9b6c6605e43fecb6b2587b9684450313ae1
Luke Boyett <lboyett@gmail.com>
2022-12-07 15:09:29 +0100

    didnt mean to make this commit

--- index.js
+++ index.js
@@ -11,12 +11,28 @@ for (let y=7; y>=0; y--) {
 }
 
 class Node {
-    constructor(position) {
+    constructor(
+        position,
+        one = null,
+        two = null,
+        three = null,
+        four = null,
+        five = null,
+        six = null,
+        seven = null,
+        eight = null
+        ) {
         this.position = position;
-        this.visited = false;
+        this.one = one;
+        this.two = two;
+        this.three = three;
+        this.four = four;
+        this.five = five;
+        this.six = six;
+        this.seven = seven;
+        this.eight = eight;
         // color a square gray if a node matching its position is created
         squares.forEach(square => {
-            this.visited = true
             if (position.toString() == square.textContent) {
                 square.style.backgroundColor = 'gray';
             }
@@ -29,14 +45,14 @@ class Tree {
         this.root = this.buildTree([x,y]);
     }
     buildTree([x,y]) {
-        if ((x>7 || x<0) || (y>7 || y<0)) return
+        if ((x>7 || x<0) || (y>7 || y<0)) return null;
 
         let bool;
         squares.forEach(square => {
-            if ((square.style.backgroundColor == 'gray') && ([x,y].toString() == square.textContent)) bool = true;
-        })
+            if ((square.style.backgroundColor == 'gray') 
+            && ([x,y].toString() == square.textContent)) bool = true;})
+        if (bool) return null;
 
-        if (bool) return;
         let node = new Node ([x,y]);
         node.one = new Tree ([x+1,y+2]);
         node.two = new Tree ([x+2,y+1]);
@@ -52,7 +68,7 @@ class Tree {
 }
 
 
-let board = new Tree ([0,0]);
+let board = new Tree ([3,3]);
 console.log(board);
 
 // if ((x>7 || x<0) || (y>7 || y<0))

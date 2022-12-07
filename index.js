08305de962efbcdd52e151bd8ff6615639a8dd75
Luke Boyett <lboyett@gmail.com>
2022-12-07 15:11:02 +0100

    merge jackson's js with luke's html chess board

--- index.js
+++ index.js
@@ -1,74 +1,115 @@
-// insert text content indicating position of square
-let counter = 0;
-const squares = document.querySelectorAll('.square')
-console.log(squares);
-let i = 0;
-for (let y=7; y>=0; y--) {
-    for (let x=0; x<=7; x++) {
-        squares[i].textContent = `${x},${y}`;
-        i++;
+let position = [3, 3];
+let board = [];
+for (let i = 0; i < 8; i++) {
+  board[i] = [];
+  for (let j = 0; j < 8; j++) {
+    board[i][j] = [i, j];
+  }
+}
+
+function labelSquares() {
+  const squares = document.querySelectorAll('.square')
+  let i = 0;
+  for (let y=7; y>=0; y--) {
+      for (let x=0; x<=7; x++) {
+          squares[i].textContent = `${x},${y}`;
+          i++;
+      }
+  }
+  return squares
+}
+let squares = labelSquares();
+function colorSquares (squares, position) {
+  squares.forEach(square => {
+    if (position.toString() == square.textContent) {
+        square.style.backgroundColor = 'gray';
     }
+})
 }
 
 class Node {
-    constructor(
-        position,
-        one = null,
-        two = null,
-        three = null,
-        four = null,
-        five = null,
-        six = null,
-        seven = null,
-        eight = null
-        ) {
-        this.position = position;
-        this.one = one;
-        this.two = two;
-        this.three = three;
-        this.four = four;
-        this.five = five;
-        this.six = six;
-        this.seven = seven;
-        this.eight = eight;
-        // color a square gray if a node matching its position is created
-        squares.forEach(square => {
-            if (position.toString() == square.textContent) {
-                square.style.backgroundColor = 'gray';
-            }
-        })
-    }
+  constructor(
+    position,
+    one = null,
+    two = null,
+    three = null,
+    four = null,
+    five = null,
+    six = null,
+    seven = null,
+    eight = null
+  ) {
+    this.position = position;
+    this.one = one;
+    this.two = two;
+    this.three = three;
+    this.four = four;
+    this.five = five;
+    this.six = six;
+    this.seven = seven;
+    this.eight = eight;
+    colorSquares(squares, position);
+  }
 }
 
 class Tree {
-    constructor([x,y]) {
-        this.root = this.buildTree([x,y]);
-    }
-    buildTree([x,y]) {
-        if ((x>7 || x<0) || (y>7 || y<0)) return null;
+  constructor(position) {
+    this.root = this.buildTree(position);
+  }
 
-        let bool;
-        squares.forEach(square => {
-            if ((square.style.backgroundColor == 'gray') 
-            && ([x,y].toString() == square.textContent)) bool = true;})
-        if (bool) return null;
-
-        let node = new Node ([x,y]);
-        node.one = new Tree ([x+1,y+2]);
-        node.two = new Tree ([x+2,y+1]);
-        node.three = new Tree ([x-1,y+2]);
-        node.four = new Tree ([x-2,y+1]);
-        node.five = new Tree ([x+1,y-2]);
-        node.six = new Tree ([x+2,y-1]);
-        node.seven = new Tree ([x-1,y-2]);
-        node.eight = new Tree ([x-2,y-1]);
-        console.log(++counter);
-        return node
-    }
+  buildTree([x, y]) {
+    if (x > 7 || x < 0 || y > 7 || y < 0) return null;
+    if (board[x][y] === 'X') return null;
+    board[x][y] = 'X';
+    let node = new Node([x, y]);
+    node.one = this.buildTree([x + 1, y + 2]);
+    node.two = this.buildTree([x + 2, y + 1]);
+    node.three = this.buildTree([x - 1, y - 2]);
+    node.four = this.buildTree([x - 2, y - 1]);
+    node.five = this.buildTree([x + 1, y - 2]);
+    node.six = this.buildTree([x - 1, y + 2]);
+    node.seven = this.buildTree([x + 2, y - 1]);
+    node.eight = this.buildTree([x - 2, y + 1]);
+    return node;
+  }
 }
 
+const tree = new Tree(position);
+console.log(tree)
 
-let board = new Tree ([3,3]);
-console.log(board);
-
-// if ((x>7 || x<0) || (y>7 || y<0))
+// ******************************************************************
+// board = [];
+// for (let i = 0; i < 8; i++) {
+//   board[i] = [];
+//   for (let j = 0; j < 8; j++) {
+//     board[i][j] = [i, j];
+//   }
+// }
+// const prettyPrint = (node, prefix = '', isLeft = true) => {
+//   if (node.one !== null) {
+//     prettyPrint(node.one, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+//   }
+//   console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.position}`);
+//   if (node.two !== null) {
+//     prettyPrint(node.two, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+//   }
+//   if (node.three !== null) {
+//     prettyPrint(node.three, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+//   }
+//   if (node.four !== null) {
+//     prettyPrint(node.four, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+//   }
+//   if (node.five !== null) {
+//     prettyPrint(node.five, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+//   }
+//   if (node.six !== null) {
+//     prettyPrint(node.six, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+//   }
+//   if (node.seven !== null) {
+//     prettyPrint(node.seven, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+//   }
+//   if (node.eight !== null) {
+//     prettyPrint(node.eight, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+//   }
+// };
+// prettyPrint(tree.buildTree(position));
